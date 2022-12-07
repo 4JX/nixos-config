84e5381f2c8b68b15432bec5cb6cf21948ea8ed1
4JX <79868816+4JX@users.noreply.github.com>
2022-12-07 23:03:02 +0100

    Gpu passthrough setup

--- system/specialisations/default.nix
+++ system/specialisations/default.nix
@@ -1,10 +1,16 @@
-{ lib, ... }:
+{ lib, pkgs, ... }:
 
 {
+
   specialisation = {
     external-display.configuration = ./multiple-displays;
     integrated-only.configuration = {
+      system.nixos.tags = [ "integrated-only" ];
       hardware.nvidiaOptimus.disable = true;
+    };
+    gpu-passthrough.configuration = {
+      system.nixos.tags = [ "gpu-passthrough" ];
+      imports = [ ./virtualisation ];
     };
   };
 }
