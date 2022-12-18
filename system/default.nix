80e5e2e9d3b442a7b0491c4cef34ced83843b7ab
4JX <79868816+4JX@users.noreply.github.com>
2022-12-18 20:11:06 +0100

    Move settings

--- system/default.nix
+++ system/default.nix
@@ -6,15 +6,22 @@
     ./specialisations
   ];
 
-  ncfg.system = {
-    flatpak.enable = true;
-    fonts = with pkgs; [ jetbrains-mono nerdfonts twemoji-color-font ];
-    pipewire = {
-      enable = true;
-      extraRates = true;
+  ncfg = {
+    system = {
+      flatpak.enable = true;
+      fonts = with pkgs; [ jetbrains-mono nerdfonts twemoji-color-font ];
+      pipewire = {
+        enable = true;
+        extraRates = true;
+      };
+      power-management.enable = true;
+      gnome-keyring.enable = true;
+      networkmanager.enable = true;
     };
-    power-management.enable = true;
-    gnome-keyring.enable = true;
-    networkmanager.enable = true;
+
+    hardware.display = {
+      colord-kde.enable = true;
+      enableHighRefreshRate = true;
+    };
   };
 }
