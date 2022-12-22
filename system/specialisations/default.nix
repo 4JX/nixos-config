83cb46deeb40483d8d36ef95aaf99fd78a345989
4JX <79868816+4JX@users.noreply.github.com>
2022-12-22 14:33:30 +0100

    Remove useless specialisation

--- system/specialisations/default.nix
+++ system/specialisations/default.nix
@@ -4,10 +4,6 @@
 
   specialisation = {
     external-display.configuration = ./multiple-displays;
-    integrated-only.configuration = {
-      system.nixos.tags = [ "integrated-only" ];
-      hardware.nvidiaOptimus.disable = true;
-    };
     gpu-passthrough.configuration = {
       system.nixos.tags = [ "gpu-passthrough" ];
 
