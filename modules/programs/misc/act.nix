df80642e0a13a8a93359378076392a803b0680b7
4JX <79868816+4JX@users.noreply.github.com>
2023-01-24 14:29:34 +0100

    Upd

---
+++ modules/programs/misc/act.nix
@@ -0,0 +1,16 @@
+{ config, lib, primaryUser, ... }:
+
+let
+  cfg = config.ncfg.programs.misc.act;
+in
+{
+  options.ncfg.programs.misc.act.enable = lib.mkEnableOption "Enable Act";
+
+  config = lib.mkIf cfg.enable {
+    virtualisation.docker.enable = true;
+
+    home-manager.users.${primaryUser} = { pkgs, ... }: {
+      home.packages = with pkgs; [ act ];
+    };
+  };
+}
--- modules/programs/misc/default.nix
+++ modules/programs/misc/default.nix
@@ -1,5 +1,8 @@
 { ... }:
 
 {
-  imports = [ ./openrgb ];
+  imports = [
+    ./openrgb
+    ./act.nix
+  ];
 }
