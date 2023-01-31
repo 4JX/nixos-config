5219556f0f67835a4d811d81abbfe9bcd98a6569
4JX <79868816+4JX@users.noreply.github.com>
2023-01-31 12:50:06 +0100

    Font updates

--- modules/system/fonts.nix
+++ modules/system/fonts.nix
@@ -1,17 +1,32 @@
-{ lib, config, ... }:
+{ lib, config, pkgs, ... }:
 
 let
-  cfg = config.ncfg.system;
+  cfg = config.ncfg.system.fonts;
+  commonFonts = with pkgs; [
+    # No tofu
+    noto-fonts
+    noto-fonts-extra
+    noto-fonts-cjk-sans
+    noto-fonts-cjk-serif
+
+    # Others
+    jetbrains-mono
+    nerdfonts
+    twemoji-color-font
+  ];
 in
 {
-  options.ncfg.system.fonts = lib.mkOption {
-    type = lib.types.listOf lib.types.path;
-    default = [ ];
-    description = lib.mdDoc "List of primary font paths.";
+  options.ncfg.system.fonts = {
+    enableCommonFonts = lib.mkEnableOption "Enable common fonts";
+
+    custom = lib.mkOption {
+      type = lib.types.listOf lib.types.path;
+      default = [ ];
+      description = lib.mdDoc "List of primary font paths.";
+    };
   };
 
   config.fonts = {
-    inherit (cfg) fonts;
-    enableDefaultFonts = true;
+    fonts = (lib.optionals cfg.enableCommonFonts commonFonts) ++ cfg.custom;
   };
 }
