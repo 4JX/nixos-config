67746111243bad9b237fe83fbee3a48911e064ec
4JX <79868816+4JX@users.noreply.github.com>
2023-02-16 18:29:28 +0100

    Gamemoderun

--- modules/programs/games/steam/default.nix
+++ modules/programs/games/steam/default.nix
@@ -1,4 +1,4 @@
-{ lib, config, pkgs, ... }:
+{ lib, config, pkgs, primaryUser, ... }:
 
 let
   cfg = config.ncfg.programs.games.steam;
@@ -7,6 +7,7 @@ in
   options.ncfg.programs.games.steam = {
     enable = lib.mkEnableOption "Enable Steam";
     enableProtonGE = lib.mkEnableOption "Enable GloriousEggroll's fork of proton";
+    enableGamemoderun = lib.mkEnableOption "Enable the gamemoderun command";
   };
 
   config = lib.mkIf cfg.enable {
@@ -14,12 +15,17 @@ in
       enable = true;
       remotePlay.openFirewall = true;
     };
+
     hardware.steam-hardware.enable = true;
 
     # Replace the desktop file if offload is enabled (force use nvidia-offload)
     # home-manager.users.${primaryUser} = lib.mkIf config.hardware.nvidia.prime.offload.enable {
     #   home.file.".local/share/applications/steam.desktop".source = ./steam.desktop;
     # };
+
+    home-manager.users.${primaryUser} = {
+      home.packages = with pkgs; lib.optionals cfg.enableGamemoderun [ gamemode ];
+    };
 
     environment.sessionVariables = lib.mkIf cfg.enableProtonGE {
       STEAM_EXTRA_COMPAT_TOOLS_PATHS = "${pkgs.proton-ge-custom}";
