b41ac85234ec02e9039153a12157f6ed76e1d098
4JX <79868816+4JX@users.noreply.github.com>
2023-05-05 20:46:12 +0200

    Kinda fix figma exporting

--- hosts/home/default.nix
+++ hosts/home/default.nix
@@ -5,6 +5,12 @@
 
   home-manager.users.${primaryUser} = { pkgs, ... }: {
 
+    # https://github.com/NixOS/nixpkgs/issues/168484#issuecomment-1501080778 Fixes crash when figma-linux tries to save files
+    xdg.systemDirs.data = with pkgs; [
+      "${gtk3}/share/gsettings-schemas/gtk+3-${gtk3.version}"
+      "${gsettings-desktop-schemas}/share/gsettings-schemas/gsettings-desktop-schemas-${gsettings-desktop-schemas.version}"
+    ];
+
     home.packages = with pkgs; [
       kate
       keepassxc
@@ -27,6 +33,7 @@
       node2nix
       cyberchef
       element-desktop
+      figma-linux
     ];
 
     home.stateVersion = "22.11";
