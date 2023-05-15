6d274977d73daf7219c481ff4c1af4e0f9e2a469
4JX <79868816+4JX@users.noreply.github.com>
2023-05-15 21:14:34 +0200

    Even more mpv stuffs

--- pkgs/mpv/default.nix
+++ pkgs/mpv/default.nix
@@ -1,6 +1,7 @@
 { pkgs, lib, ... }:
 
 let
+  # This is a very convoluted fetchurl, but has more info so that it can be more easily converted into a proper package
   singleFileScript = inputs@{ pname, rev, filename, url, sha256, meta }: pkgs.callPackage
     (
       import ./single-file.nix inputs
@@ -41,5 +42,25 @@ in
     pauseWhenMinimize = pkgs.callPackage ./pauseWhenMinimize.nix { };
   };
 
-  anime4k = pkgs.callPackage ./an4k.nix { };
+  shaders = {
+    # https://gist.github.com/igv
+    SSimSuperRes = pkgs.fetchurl {
+      url = "https://gist.githubusercontent.com/igv/2364ffa6e81540f29cb7ab4c9bc05b6b/raw/15d93440d0a24fc4b8770070be6a9fa2af6f200b/SSimSuperRes.glsl";
+      sha256 = "sha256-qLJxFYQMYARSUEEbN14BiAACFyWK13butRckyXgVRg8=";
+    };
+
+    SSimDownscaler = pkgs.fetchurl {
+      url = "https://gist.githubusercontent.com/igv/36508af3ffc84410fe39761d6969be10/raw/6998ff663a135376dbaeb6f038e944d806a9b9d9/SSimDownscaler.glsl";
+      sha256 = "sha256-FF3rw4DzwgWLHSkziQXSvIBgAxVtZ0skdFJ03S1QmQY=";
+    };
+
+    KrigBilateral = pkgs.fetchurl {
+      url = "https://gist.githubusercontent.com/igv/a015fc885d5c22e6891820ad89555637/raw/7c151e0af2281ae6657809be1f3c5efe0e325c38/KrigBilateral.glsl";
+      sha256 = "sha256-uIbPX59nIHeHC9wa1Mv1nQartUusOgXaEHQyA95BST8=";
+    };
+
+    anime4k = pkgs.callPackage ./an4k.nix { };
+
+    fsrcnnx = pkgs.callPackage ./fsrcnnx.nix { };
+  };
 }
---
+++ pkgs/mpv/fsrcnnx.nix
@@ -0,0 +1,51 @@
+{ lib, stdenvNoCC, fetchurl, variant ? "8-0-4-1", pkgs }:
+
+
+# let
+#   variants = [ "8-0-4-1" "8-0-4-1_Lineart" "16-0-4-1" ];
+#   insetallString = variant: (file: "install -Dm644 ${file} $out/${file}") "FSRCNNX_x2_${variant}.glsl";
+# in
+stdenvNoCC.mkDerivation rec {
+  pname = "fsrcnnx";
+  version = "1.1";
+
+  src = fetchurl {
+    name = "important";
+    url = "https://github.com/igv/FSRCNN-TensorFlow/releases/download/1.1/checkpoints_params.7z";
+    sha256 = "sha256-h5B7DU0W5B39rGaqC9pEqgTTza5dKvUHTFlEZM1mfqo=";
+  };
+  src8 = fetchurl {
+    url = "https://github.com/igv/FSRCNN-TensorFlow/releases/download/${version}/FSRCNNX_x2_8-0-4-1.glsl";
+    sha256 = "sha256-6ADbxcHJUYXMgiFsWXckUz/18ogBefJW7vYA8D6Nwq4=";
+  };
+  src16 = fetchurl {
+    url = "https://github.com/igv/FSRCNN-TensorFlow/releases/download/${version}/FSRCNNX_x2_16-0-4-1.glsl";
+    sha256 = "sha256-1aJKJx5dmj9/egU7FQxGCkTCWzz393CFfVfMOi4cmWU=";
+  };
+
+  nativeBuildInputs = [ pkgs.p7zip ];
+
+  unpackPhase = ''
+    7z -aoa x ${src}
+  '';
+
+  installPhase = ''
+    runHook preInstall
+
+    ls -lah
+
+    install -Dm644 ${src8} $out/FSRCNNX_x2_8-0-4-1.glsl
+    install -Dm644 FSRCNNX_x2_8-0-4-1_LineArt.glsl $out/FSRCNNX_x2_8-0-4-1_LineArt.glsl
+    install -Dm644 ${src16} $out/FSRCNNX_x2_16-0-4-1.glsl
+
+    runHook postInstall
+  '';
+
+  meta = with lib; {
+    description = "An implementation of the Fast Super-Resolution Convolutional Neural Network in TensorFlow";
+    homepage = "https://github.com/igv/FSRCNN-TensorFlow";
+    license = licenses.lgpl3Plus;
+    maintainers = with maintainers; [ lunik1 ];
+    platforms = platforms.all;
+  };
+}
