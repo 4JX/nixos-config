27b7f5bf4c8ae735b9c264c29201cf50bd54d376
4JX <79868816+4JX@users.noreply.github.com>
2023-06-03 19:10:15 +0200

    Slightly working portmaster-core
    Does work when specifying some data dir and using the ui at localhost:817 in devmode but still far from ideal setup

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -6,5 +6,6 @@
   proton-ge-custom = pkgs.callPackage ./proton-ge-custom.nix { };
   proton-ge-custom-621 = pkgs.callPackage ./proton-ge-custom-6.21-2.nix { };
   vimix-cursor-theme = pkgs.callPackage ./vimix-cursors.nix { };
+  portmaster = pkgs.callPackage ./portmaster.nix { };
 }
   // (lib.recursiveUpdate { mpv = pkgs.callPackage ./mpv { }; } (pkgs.callPackage ./pull-requests { }))
