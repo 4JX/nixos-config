3cee0ae1774d0d70cefc6dd5e9838d4412d55942
4JX <79868816+4JX@users.noreply.github.com>
2023-06-07 17:53:33 +0200

    Change GTK theme

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -8,6 +8,7 @@ myLib.recursiveMergeAttrs
       proton-ge-custom-621 = pkgs.callPackage ./proton-ge-custom-6.21-2.nix { };
       vimix-cursor-theme = pkgs.callPackage ./vimix-cursors.nix { };
       portmaster = pkgs.callPackage ./portmaster.nix { };
+      mono-gtk-theme = pkgs.callPackage ./mono-gtk-theme.nix { };
     }
 
     (pkgs.callPackage ./pull-requests { })
