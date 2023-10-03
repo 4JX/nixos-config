eceef00afa2039ed320b08b2a07f69aa357f8f07
4JX <79868816+4JX@users.noreply.github.com>
2023-10-03 15:06:20 +0200

    Wireshark

---
+++ modules/programs/networking/sniffers/default.nix
@@ -0,0 +1,7 @@
+{ ... }:
+
+{
+  imports = [
+    ./wireshark.nix
+  ];
+}
---
+++ modules/programs/networking/sniffers/wireshark.nix
@@ -0,0 +1,25 @@
+{ primaryUser, config, lib, pkgs, ... }:
+
+let
+  cfg = config.ncfg.programs.networking.p2p.qBittorrent;
+in
+{
+  options.ncfg.programs.networking.sniffers.wireshark = {
+    enable = lib.mkEnableOption "Enable wireshark";
+  };
+
+  config = lib.mkIf cfg.enable {
+
+    users.users.${primaryUser} = {
+      extraGroups = [ "wireshark" ];
+    };
+
+    programs = {
+      wireshark = {
+        enable = true;
+        package = pkgs.wireshark;
+      };
+    };
+
+  };
+}
