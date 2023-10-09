997a451d1c99efc89992aee78d7cd86a349073c4
4JX <79868816+4JX@users.noreply.github.com>
2023-10-09 22:06:39 +0200

    Add udev rule for the laptop's RGB

--- hosts/laptop/default.nix
+++ hosts/laptop/default.nix
@@ -26,6 +26,10 @@
     };
   };
 
+  services.udev.extraRules = ''
+    SUBSYSTEM=="usb", ATTR{idVendor}=="048d", ATTR{idProduct}=="c965", MODE="0666"
+  '';
+
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It‘s perfectly fine and recommended to leave
