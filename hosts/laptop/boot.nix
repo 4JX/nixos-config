f335086e2c9d497680832bae012da92bdd4c8cd0
4JX <79868816+4JX@users.noreply.github.com>
2023-10-12 23:49:12 +0200

    Update inputs

--- hosts/laptop/boot.nix
+++ hosts/laptop/boot.nix
@@ -1,7 +1,7 @@
 { pkgs, config, ... }: {
   # Use the grub bootloader.
   boot = {
-    kernelPackages = pkgs.linuxKernel.packages.linux_6_4;
+    kernelPackages = pkgs.linuxKernel.packages.linux_6_5;
     blacklistedKernelModules = [ "nouveau" ];
     # extraModulePackages = with config.boot.kernelPackages; [ lenovo-legion-module ];
 
