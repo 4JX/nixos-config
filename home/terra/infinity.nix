68f7a0f849f55b4d4e638b39f175ecc8e24ce834
4JX <79868816+4JX@users.noreply.github.com>
2023-10-22 16:32:05 +0200

    Move easyeffects

--- home/terra/infinity.nix
+++ home/terra/infinity.nix
@@ -1,9 +1,25 @@
 { pkgs, ... }:
 
 {
+  imports = [ ../modules ];
+
   # https://github.com/NixOS/nixpkgs/issues/168484#issuecomment-1501080778 Fixes crash when figma-linux tries to save files
   xdg.systemDirs.data = with pkgs; [
     "${gtk3}/share/gsettings-schemas/gtk+3-${gtk3.version}"
     "${gsettings-desktop-schemas}/share/gsettings-schemas/gsettings-desktop-schemas-${gsettings-desktop-schemas.version}"
   ];
+
+  ncfg = {
+    programs = {
+      audio.easyeffects = {
+        enable = true;
+        outputPresets = {
+          # https://github.com/wwmm/easyeffects/wiki/Community-Presets
+          # Based on https://gist.github.com/sebastian-de/648555c1233fdc6688c0a224fc2fca7e
+          "Legion 5 Pro" = ../easyeffects/L5P.json;
+          "DT880" = ../easyeffects/DT880.json;
+        };
+      };
+    };
+  };
 }
