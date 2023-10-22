5c96119b66fe935669c795808dc21f0941b92146
4JX <79868816+4JX@users.noreply.github.com>
2023-10-22 16:25:42 +0200

    User+host specific home-manager config skeleton

--- modules/options/system/default.nix
+++ modules/options/system/default.nix
@@ -1,5 +1,8 @@
-{ ... }:
+{ lib, config, ... }:
 
+let
+  inherit (lib) mkOption types;
+in
 {
   imports = [
     ./fonts
@@ -12,4 +15,21 @@
     ./power-management.nix
     ./systemd.nix
   ];
+
+  config.assertions = [{
+    assertion = config.ncfg.system.users != [ ];
+    message =
+      ''
+        No users have been declared for the system.
+        Consider setting `config.ncfg.system.users` in your configuration
+      '';
+  }];
+
+  options.ncfg.system = {
+    users = mkOption {
+      type = with types; listOf str;
+      default = [ "infinity" ];
+      description = "A list of home-manager users on the system.";
+    };
+  };
 }
