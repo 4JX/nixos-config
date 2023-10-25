7c345a912f3f75344cdafea50c1c2b76cf253e8f
4JX <79868816+4JX@users.noreply.github.com>
2023-10-26 00:30:19 +0200

    Shell updates

--- home/modules/system/terminal/shell/zsh.nix
+++ home/modules/system/terminal/shell/zsh.nix
@@ -21,23 +21,45 @@
       eza = "${eza}/bin/eza --icons";
       scrcpy = "${scrcpy}/bin/scrcpy --bit-rate 32M --encoder 'OMX.qcom.video.encoder.avc' --window-title 'Phone' --stay-awake --turn-screen-off";
       discordrpc = "ln -sf {app/com.discordapp.Discord,$XDG_RUNTIME_DIR}/discord-ipc-0";
+
+      # Faster navigation
+      ".." = "cd ..";
+      "..." = "cd ../../";
+      "...." = "cd ../../../";
+      "....." = "cd ../../../../";
+      "......" = "cd ../../../../../";
     };
 
     plugins = [
       {
         name = "fast-syntax-highlighting";
-        file = "fast-syntax-highlighting.plugin.zsh";
-        src = "${pkgs.zsh-fast-syntax-highlighting}/share/zsh/site-functions";
+        file = "share/zsh/site-functions/fast-syntax-highlighting.plugin.zsh";
+        src = "${pkgs.zsh-fast-syntax-highlighting}";
       }
       {
         name = "zsh-nix-shell";
-        file = "nix-shell.plugin.zsh";
-        src = "${pkgs.zsh-nix-shell}/share/zsh-nix-shell";
+        file = "share/zsh-nix-shell/nix-shell.plugin.zsh";
+        src = "${pkgs.zsh-nix-shell}";
       }
       {
         name = "nix-zsh-shell-completions";
-        file = "nix-zsh-completions.plugin.zsh";
-        src = "${pkgs.nix-zsh-completions}/share/zsh/site-functions";
+        file = "share/zsh/plugins/nix/nix-zsh-completions.plugin.zsh";
+        src = "${pkgs.nix-zsh-completions}";
+      }
+      {
+        name = "nix-zsh-shell-completions";
+        file = "share/zsh/plugins/nix/nix-zsh-completions.plugin.zsh";
+        src = "${pkgs.nix-zsh-completions}";
+      }
+      {
+        name = "zsh-autopair";
+        file = "zsh-autopair.plugin.zsh";
+        src = pkgs.fetchFromGitHub {
+          owner = "hlissner";
+          repo = "zsh-autopair";
+          rev = "34a8bca0c18fcf3ab1561caef9790abffc1d3d49";
+          sha256 = "1h0vm2dgrmb8i2pvsgis3lshc5b0ad846836m62y8h3rdb3zmpy1";
+        };
       }
       {
         name = "zsh-colored-man-pages";
