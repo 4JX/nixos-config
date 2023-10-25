1a7b1783519893e86b8bb9539811eeaa5c01e02b
4JX <79868816+4JX@users.noreply.github.com>
2023-10-25 21:15:47 +0200

    stateVersion docs

--- home/terra/infinity.nix
+++ home/terra/infinity.nix
@@ -3,6 +3,9 @@
 {
   imports = [ ../modules ];
 
+  # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
+  home.stateVersion = "22.11";
+
   # https://github.com/NixOS/nixpkgs/issues/168484#issuecomment-1501080778 Fixes crash when figma-linux tries to save files
   xdg.systemDirs.data = with pkgs; [
     "${gtk3}/share/gsettings-schemas/gtk+3-${gtk3.version}"
--- hosts/terra/default.nix
+++ hosts/terra/default.nix
@@ -31,11 +31,6 @@
     SUBSYSTEM=="usb", ATTR{idVendor}=="048d", ATTR{idProduct}=="c965", MODE="0666"
   '';
 
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "22.05"; # Did you read the comment?
+  # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
+  system.stateVersion = "22.05";
 }
