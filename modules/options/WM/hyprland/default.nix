467cf7fc1dceefe08aaef59292f81e93dd9e7663
4JX <79868816+4JX@users.noreply.github.com>
2023-10-28 17:04:23 +0200

    Update hyprland module

--- modules/options/WM/hyprland/default.nix
+++ modules/options/WM/hyprland/default.nix
@@ -1,12 +1,13 @@
-{ inputs, lib, config, mainUser, ... }:
+{ inputs, lib, config, pkgs, ... }:
 
 let
   cfg = config.ncfg.WM.hyprland;
 in
 {
-  imports = [ inputs.hyprland.nixosModules.default ];
-
   options.ncfg.WM.hyprland.enable = lib.mkEnableOption "hyprland";
+
+  # Disables Nixpkgs Hyprland module to avoid conflicts
+  # disabledModules = [ "programs/hyprland.nix" ];
 
   config = lib.mkIf cfg.enable {
     nix.settings = {
@@ -14,17 +15,11 @@ in
       trusted-public-keys = [ "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc=" ];
     };
 
+    # Should probably just gut out the relevant options from here but eh
     programs.hyprland = {
       enable = true;
-      enableNvidiaPatches = true;
-    };
-
-    home-manager.users.${mainUser} = { pkgs, ... }: {
-      xdg.configFile."hyprland.conf" = {
-        text = import ./config.nix { };
-        target = "hypr/hyprland.conf";
-        onChange = "${pkgs.hyprland}/bin/hyprctl reload";
-      };
+      # Because I currently use upstream hyprland
+      portalPackage = inputs.xdg-portal-hyprland.packages.${pkgs.system}.xdg-desktop-portal-hyprland;
     };
   };
 }
