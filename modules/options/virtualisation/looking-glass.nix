9272d88acd72e1fed1511f0584c5fe07bed4fd7a
4JX <79868816+4JX@users.noreply.github.com>
2023-10-30 18:46:11 +0100

    Remove mainUser

--- modules/options/virtualisation/looking-glass.nix
+++ modules/options/virtualisation/looking-glass.nix
@@ -1,4 +1,4 @@
-{ config, lib, pkgs, mainUser, ... }:
+{ config, lib, pkgs, ... }:
 
 # Config for looking-glass is handled on the home-manager side
 let
@@ -8,21 +8,39 @@ in
   options.ncfg.virtualisation.looking-glass = {
     enable = lib.mkEnableOption "the Looking Glass client";
 
-    createSharedMemoryFile = lib.mkOption {
-      default = cfg.enable;
-      type = lib.types.bool;
-      description = "Automatically create a shared memory file for Looking Glass to use";
+    sharedMemoryFile = {
+      create = lib.mkOption {
+        default = true;
+        type = lib.types.bool;
+        description = "Automatically create a shared memory file for Looking Glass to use";
+      };
+
+      user = lib.mkOption {
+        default = "";
+        type = lib.types.str;
+        description = "The owner of the shared memory file";
+      };
     };
   };
 
   config = lib.mkIf cfg.enable {
+    assertions = [{
+      # If the file is to be automatically created it needs to have a non-empty user
+      assertion = (!cfg.enable) || cfg.sharedMemoryFile.user != "";
+      message =
+        ''
+          The shared memory file for looking glass needs an user to be specified.
+          Consider setting `config.ncfg.virtualisation.looking-glass.sharedMemoryFile.user` in your configuration
+        '';
+    }];
+
     environment.systemPackages = with pkgs; [
       looking-glass-client
     ];
 
-    ncfg.virtualisation.sharedMemoryFiles = lib.optionals cfg.createSharedMemoryFile {
+    ncfg.virtualisation.sharedMemoryFiles = lib.optionals cfg.sharedMemoryFile.create {
       looking-glass = {
-        user = mainUser;
+        user = cfg.sharedMemoryFile.user;
         group = "qemu-libvirtd";
         mode = "666";
       };
