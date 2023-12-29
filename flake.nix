631ae94a72d90027ce07d8c8925fb02ac302cd77
4JX <79868816+4JX@users.noreply.github.com>
2023-12-29 01:02:45 +0100

    pain

--- flake.nix
+++ flake.nix
@@ -12,7 +12,13 @@
     });
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/282d9cd278480206964640c73cab2621e77300d7";
+    nixpkgs.url = "github:4JX/nixpkgs/egui2";
+    # nixpkgs.url = "github:nixos/nixpkgs/76d4d2e76b2b8281aa297d164a9eb410bf81319a";
+    # Bad
+    # nixpkgs.url = "github:nixos/nixpkgs/a9e00367e77d2c0b9b6f7daf9abd0fe136f3cc57";
+    # nixpkgs.url = "github:nixos/nixpkgs/4ed0fc839a30003f759e12898961747688383105";
+    # Good
+    # nixpkgs.url = "github:nixos/nixpkgs/c7e70840c0ea981bc97a265b2294246dcc33775d";
 
     nixos-hardware.url = "github:nixos/nixos-hardware";
 
