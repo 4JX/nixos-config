bb35ac36af37071f3c917bbac6bb0c97ad870137
4JX <79868816+4JX@users.noreply.github.com>
2024-02-10 23:56:59 +0100

    Add more fonts

--- modules/system/fonts/default.nix
+++ modules/system/fonts/default.nix
@@ -13,6 +13,22 @@ let
     jetbrains-mono
     (nerdfonts.override { fonts = [ "NerdFontsSymbolsOnly" ]; })
     twemoji-color-font
+
+    # Stolen from 
+    # https://github.com/PatrickShaw/patrickshaw/blob/799ab4a839d092869bf8f6a55cb3afb1d0cd0a49/personal/dotfiles/.config/fonts/conf.d/flake.nix
+    # https://github.com/somasis/nixos/blob/3519e7df91f19106c779bd71c305ad82a473850e/users/somasis/desktop/study/writing.nix#L166-L172
+    # See: https://dribbble.com/stories/2021/04/26/web-design-data-fonts
+    # See: https://jichu4n.com/posts/the-most-popular-fonts-on-the-web-a-study/
+    caladea # Cambria
+    carlito # Calibri
+    comic-relief # Comic Sans MS
+    gelasio # Georgia
+    liberation-sans-narrow # Arial Narrow
+    liberation_ttf # Arial, Helvetica, Times New Roman, Courier New
+    noto-fonts-extra # Arial, Times New Roman
+    roboto
+    open-sans
+    font-awesome
   ];
 in
 {
@@ -27,6 +43,17 @@ in
       type = lib.types.listOf lib.types.path;
       default = [ ];
       description = lib.mdDoc "List of primary font paths.";
+    };
+
+    fontconfig = {
+      enable = true;
+
+      defaultFonts = {
+        # sansSerif = [ "Inter" "Noto Sans" ];
+        # serif = [ "Noto Serif" ];
+        # monospace = [ "JetBrains Mono" "Noto Sans Mono" ];
+        emoji = [ "Twitter Color Emoji" ];
+      };
     };
   };
 
