1d3a6139477b1867ac27940cb19dc0faf1cf8bd5
4JX <79868816+4JX@users.noreply.github.com>
2024-04-17 19:18:09 +0200

    Remove flatpak font binding
    See https://github.com/NixOS/nixpkgs/pull/262462

--- modules/system/flatpak.nix
+++ modules/system/flatpak.nix
@@ -1,4 +1,4 @@
-{ lib, config, pkgs, ... }:
+{ lib, config, ... }:
 
 let
   cfg = config.ncfg.system.flatpak;
@@ -12,27 +12,5 @@ in
       services.flatpak.enable = true;
 
       xdg.portal.enable = true;
-
-      # https://github.com/NixOS/nixpkgs/issues/119433
-      # https://github.com/accelbread/config-flake/blob/744196b43b93626025e5a2789c8700a5ec371aad/nix/nixosModules/bind-fonts-icons.nix#L9
-      system.fsPackages = [ pkgs.bindfs ];
-      fileSystems =
-        let
-          mkRoSymBind = path: {
-            device = path;
-            fsType = "fuse.bindfs";
-            options = [ "ro" "resolve-symlinks" "x-gvfs-hide" ];
-          };
-          aggregatedFonts = pkgs.buildEnv {
-            name = "system-fonts";
-            paths = config.fonts.packages;
-            pathsToLink = [ "/share/fonts" ];
-          };
-        in
-        {
-          # Create an FHS mount to support flatpak host icons/fonts
-          "/usr/share/icons" = mkRoSymBind (config.system.path + "/share/icons");
-          "/usr/share/fonts" = mkRoSymBind (aggregatedFonts + "/share/fonts");
-        };
     };
 }
