0bd03a126470c82cae6cdd117df1bfce4b545b6c
4JX <79868816+4JX@users.noreply.github.com>
2024-11-01 22:20:16 +0100

    refactor(pkgs): remove singleFileScript

--- pkgs/mpv/default.nix
+++ pkgs/mpv/default.nix
@@ -1,44 +1,9 @@
-{ pkgs, lib, ... }:
+{ pkgs, ... }:
 
-let
-  # This is a very convoluted fetchurl, but has more info so that it can be more easily converted into a proper package
-  singleFileScript = inputs@{ pname, rev, filename, url, sha256, meta }: pkgs.callPackage
-    (
-      import ./single-file.nix inputs
-    )
-    { };
-in
 {
   mpvScripts = {
-    betterChapters = singleFileScript rec {
-      pname = "mpv-betterChapters";
-      rev = "1d1daf22f0ec5f0219e4e72216e772828f5c8e4c";
-      filename = "betterchapters.lua";
-      url = "https://gist.githubusercontent.com/Hakkin/4f978a5c87c31f7fe3ae/raw/${rev}/${filename}";
-      sha256 = "sha256-bPH9sm/aO/FnWYY8K+2DFjFahaOCrSj5Es2bqfBk9ow=";
-      meta = with lib; {
-        description = "Loads the next or previous playlist entry if there are no more chapters in the seek direction.";
-        homepage = "https://gist.github.com/Hakkin/4f978a5c87c31f7fe3ae";
-        license = licenses.unfree;
-        # mantainers = ;
-      };
-    };
-
-    # Pay attention to https://github.com/po5/evafast/pull/8
-    evafast = singleFileScript rec {
-      pname = "mpv-evafast";
-      rev = "f9ee7e41dedf0f65186900e0ccdd6ca6a8ced7ed";
-      filename = "evafast.lua";
-      url = "https://raw.githubusercontent.com/po5/evafast/${rev}/${filename}";
-      sha256 = "sha256-UkqWhHws0V7rV5f2w7g3d0jHvEP70zhbomE8AQIiwh0=";
-      meta = with lib; {
-        description = "mpv script for hybrid fastforward and seeking";
-        homepage = "https://github.com/po5/evafast";
-        license = licenses.unfree;
-        # mantainers = ;
-      };
-    };
-
+    betterChapters = pkgs.callPackage ./betterchapters.nix { };
+    evafast = pkgs.callPackage ./evafast.nix { };
     pauseWhenMinimize = pkgs.callPackage ./pauseWhenMinimize.nix { };
     mpv-jellyfin = pkgs.callPackage ./mpv-jellyfin.nix { };
   };
@@ -59,8 +24,6 @@ in
       url = "https://gist.githubusercontent.com/igv/a015fc885d5c22e6891820ad89555637/raw/7c151e0af2281ae6657809be1f3c5efe0e325c38/KrigBilateral.glsl";
       sha256 = "sha256-uIbPX59nIHeHC9wa1Mv1nQartUusOgXaEHQyA95BST8=";
     };
-
-    anime4k = pkgs.callPackage ./an4k.nix { };
 
     fsrcnnx = pkgs.callPackage ./fsrcnnx.nix { };
   };
---
+++ pkgs/mpv/evafast.nix
@@ -0,0 +1,37 @@
+{ lib, stdenvNoCC, fetchurl, ... }:
+
+let
+  filename = "evafast.lua";
+  rev = "f9ee7e41dedf0f65186900e0ccdd6ca6a8ced7ed";
+  shortRev = builtins.substring 0 8 rev;
+in
+stdenvNoCC.mkDerivation rec {
+  pname = "mpv-evafast";
+  version = shortRev;
+
+  # Pay attention to https://github.com/po5/evafast/pull/8
+  src = fetchurl {
+    url = "https://raw.githubusercontent.com/po5/evafast/${rev}/${filename}";
+    sha256 = "sha256-UkqWhHws0V7rV5f2w7g3d0jHvEP70zhbomE8AQIiwh0=";
+  };
+
+  dontBuild = true;
+  dontUnpack = true;
+
+  installPhase = ''
+    runHook preInstall
+
+    install -Dm644 ${src} $out/share/mpv/scripts/${filename}
+
+    runHook postInstall
+  '';
+
+  passthru.scriptName = filename;
+
+  meta = with lib; {
+    description = "mpv script for hybrid fastforward and seeking";
+    homepage = "https://github.com/po5/evafast";
+    license = licenses.unfree;
+    # maintainers = ;
+  };
+}
