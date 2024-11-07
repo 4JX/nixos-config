618e89a3804d3e0137e327db98d7fb19c9f0fcfb
4JX <4JXcYvmyu3Hz8fV@protonmail.com>
2024-11-07 14:15:39 +0100

    feat(localsend@terra): Open ports for localsend

--- hosts/terra/users/infinity.nix
+++ hosts/terra/users/infinity.nix
@@ -107,4 +107,11 @@
     OVMF
     pciutils
   ];
+
+  # Allow localsend to receive files
+  # https://github.com/localsend/localsend?tab=readme-ov-file#setup
+  networking.firewall = {
+    allowedTCPPorts = [ 53317 ];
+    allowedUDPPorts = [ 53317 ];
+  };
 }
