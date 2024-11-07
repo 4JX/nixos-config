1f7af66fd8447248825b76735ffb6afa6711db53
4JX <79868816+4JX@users.noreply.github.com>
2024-11-07 14:15:39 +0100

    feat(localsend@terra): Open ports for localsend

--- hosts/terra/users/infinity.nix
+++ hosts/terra/users/infinity.nix
@@ -105,4 +105,11 @@
     OVMF
     pciutils
   ];
+
+  # Allow localsend to receive files
+  # https://github.com/localsend/localsend?tab=readme-ov-file#setup
+  networking.firewall = {
+    allowedTCPPorts = [ 53317 ];
+    allowedUDPPorts = [ 53317 ];
+  };
 }
